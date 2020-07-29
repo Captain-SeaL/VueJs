abef3a729d5aa741b978c025963e45e43f98c1ab
Captain-SeaL <pimenovpavel98@gmail.com>
2020-07-29 23:34:24 +0300

    add vuex

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/Vuex">Vuex</router-link>
     </div>
     <transition v-on:enter="enter" v-on:leave="leave" v-bind:css="false" appear>
     <router-view></router-view>
@@ -18,15 +18,18 @@ import {TimelineMax, /*TweenMax*/} from 'gsap';
 
 export default {
   mounted(){
-    let App = document.querySelector('#app');
-    let tl = new TimelineMax();
-    tl.fromTo(App, 1 , {y:2000},{y:0});
-    tl.to(document.querySelector("#vuelogo"), 2 , {rotationY:180,repeat:-1});
+    //let App = document.querySelector('#app');
+    //let tl = new TimelineMax();
+    //tl.fromTo(App, 1 , {y:2000},{y:0},1);
   },
   methods:{
     enter(){
         let tl = new TimelineMax();
+        tl.fromTo(document.querySelector("#app"),1,{x:-2000},{x:0});
         tl.to(document.querySelector("#vuelogo"), 2 , {rotationY:180,repeat:-1});
+    },
+    leave(){
+        
     }
   }
 }
---
+++ src/views/Vuex.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="vuex">
+    <img alt="Vue logo" id="vuelogo" src="../assets/logo.png">
+    <h1>This is an example of Vuex using.</h1>
+    <h2>Count of posts is {{ postsCount }} </h2>
+    <PostForm/>
+    <hr>
+    <div class="post" v-for="post in allPosts" :key="post.id" >
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import PostForm from '../components/PostForm'
+export default {
+  name: "vuex",
+  computed: mapGetters(['allPosts', 'postsCount']),
+  
+  methods: mapActions(['fetchPosts']),
+  components: { PostForm },
+  async mounted() {
+    //this.$store.dispatch('fetchPosts')
+    this.fetchPosts();
+  }
+}
+</script>
+<style scoped>
+
+.post{
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  border-bottom: 1rem;
+}
+</style>
